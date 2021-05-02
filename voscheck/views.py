6a60d47393d946bb91dcc85df21a1afb62322e6e
Rennes <leienshu@126.com>
2021-05-02 11:45:39 +0800

    设置登陆页面

--- voscheck/views.py
+++ voscheck/views.py
@@ -4,5 +4,13 @@ from django.views import generic
 # class IndexView(generic.FormView):
 #     template_name = 'voscheck/index.html'
 def index(request):
-    print('hello')
     return render(request, 'voscheck/index.html')
+
+def login(request):
+    return render(request, 'voscheck/login.html')
+
+def logout(request):
+    if not request.session.get('is_login', None):
+        return redirect("login/")
+    request.session.flush()
+    redirect("login/")
