b7ea9331f175bd28f19e085f656bf4b00077fa66
Albert Lei <albert.lei@hotmail.com>
2022-02-12 13:25:04 +0800

    取消几个必填字段

--- accounts/models.py
+++ accounts/models.py
@@ -9,9 +9,9 @@ class Account(models.Model):
         ]
     zhanghao = models.CharField('账号',max_length=40,unique=True)
     zhanghu = models.CharField('账户名称',max_length=40)
-    balance = models.FloatField('账户余额')
-    credit_amount = models.FloatField('透支额度') 
-    today_cost = models.FloatField('今日消费')
+    balance = models.FloatField('账户余额',blank=True, null=True)
+    credit_amount = models.FloatField('透支额度',blank=True, null=True) 
+    today_cost = models.FloatField('今日消费',blank=True, null=True)
     kh_type = models.CharField('账户类型',max_length=20, choices=KH_CHOICES)
     created_at = models.DateTimeField('上传时间',auto_now_add=True)
     updated_at = models.DateTimeField('更新时间',auto_now=True)
